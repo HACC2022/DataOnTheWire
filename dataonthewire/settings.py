f2e75ae11b1b00c37e88bffd6f5e6f01b50104ce
Bjarne Martens <bjarne.martens@otto.de>
2022-10-19 12:40:51 -1000

    Finishing Register

--- dataonthewire/settings.py
+++ dataonthewire/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
     'input.apps.InputConfig',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -119,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
