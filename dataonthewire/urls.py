385258a806e15920da57db19a4334ee158f5a2f9
Bjarne Martens <bjarne.martens@nordakademie.de>
2022-10-25 16:17:52 -1000

    Changed default page to login

--- dataonthewire/urls.py
+++ dataonthewire/urls.py
@@ -21,7 +21,7 @@ from users import views as user_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('input/', include('input.urls')),
-    path('', user_views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('register/', user_views.register, name='register'),
+    path('', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
