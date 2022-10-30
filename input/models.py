788dfef9b7c7bbebcc567574cb4ec68776526aed
Alyssa <alawton1@my.hpu.edu>
2022-10-30 10:25:01 -1000

    Added project_fact_sheet.html

--- input/models.py
+++ input/models.py
@@ -1,8 +1,8 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 # Create your models here.
-
-
 class ProjectFactSheet(models.Model):
     prePlan = "Pre-Planning"
     plan = "Planning"
@@ -64,15 +64,15 @@ class ProjectFactSheet(models.Model):
         (of, "Other Funds"),
     )
 
-    project_ID = models.CharField(max_length=5)
+    project_ID = models.CharField(primary_key=True, max_length=5)
     project_Phase = models.CharField(max_length=50, choices=Project_Phase_Type)
     source_of_Funding = models.CharField(max_length=50, choices=Source_of_Funding_Type)
-    funding_Received = models.DecimalField(max_digits=12, decimal_places=2)
-    year_Funding_Received = models.IntegerField()
+    funding_Received = models.DecimalField(max_digits=12, decimal_places=2, validators=[MinValueValidator(0.01)])
+    year_Funding_Received = models.PositiveIntegerField(validators=[MinValueValidator(1900), MaxValueValidator(9999)])
     project_Status_Update = models.TextField()
-    links_report_studies = models.URLField(max_length=250)
-    links_images = models.FileField(upload_to='linkImages/')
-    inputPOC_lastname = models.CharField(max_length=100)
-    inputPOC_firstname = models.CharField(max_length=100)
-    inputPOC_email = models.EmailField(max_length=254)
-    inputDate = models.DateField(auto_now_add=True)
+    links_Report_Studies = models.URLField(max_length=250)
+    links_Images = models.FileField(upload_to='linkImages/')
+    input_POC_Last_Name = models.CharField(max_length=100)
+    input_POC_First_Name = models.CharField(max_length=100)
+    input_POC_Email = models.EmailField(max_length=254)
+    input_Date = models.DateField(auto_now_add=True)
--- input/views.py
+++ input/views.py
@@ -1,18 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from input.forms import ProjectFactSheetForm
 
+
 # Create your views here.
-
-
 def project_fact_sheet_view(request):
     context = {}
-    # create object of form
-    form = ProjectFactSheetForm(request.POST or None, request.FILES or None)
+    if request.method == 'POST':
+        # create object of form
+        form = ProjectFactSheetForm(request.POST or None, request.FILES or None)
 
-    # check if form data is valid
-    if form.is_valid():
-        # save the form data to model
-        form.save()
-    context['form'] = form
-    return render(request, "input/project_fact_sheet.html", {'form': form})
+        # check if form data is valid
+        if form.is_valid():
+            # save the form data to model
+            project_fact_sheet = form.save()
+            project_fact_sheet.save()
+            # context['form'] = form
+            return redirect('login')
+    else:
+        form = ProjectFactSheetForm()
+    return render(request, 'input/project_fact_sheet.html', {'form': form})
