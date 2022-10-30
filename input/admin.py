bc457371fde0b43fa9e1f4792cedb70f5a99228b
Alyssa <alawton1@my.hpu.edu>
2022-10-30 13:19:04 -1000

    Registered ProjectFactSheetAdmin

--- input/admin.py
+++ input/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from input.models import ProjectFactSheet
+
 
 # Register your models here.
+class ProjectFactSheetAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(ProjectFactSheet, ProjectFactSheetAdmin)
