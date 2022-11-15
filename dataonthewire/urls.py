1c89adbd2787e40398afcbeb5da0c99a391859cd
Bjarne Martens <bjarne.martens@nordakademie.de>
2022-11-15 10:22:18 -1000

    Added Option to download Csv

--- dataonthewire/urls.py
+++ dataonthewire/urls.py
@@ -17,13 +17,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from users import views as user_views
+from input import views as input_views
 
 urlpatterns = [
-    path('hpuadmin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('input/', include('input.urls')),
     path('register/', user_views.register, name='register'),
     path('', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('admin/', user_views.admin, name='admin'),
+    path('hpuadmin/', user_views.admin, name='admin'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('hpuadmin/export-to-csv/', input_views.export_to_csv, name='export-to-csv')
 ]
--- input/views.py
+++ input/views.py
@@ -1,10 +1,52 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from input.models import ProjectFactSheet
 from django.http import HttpResponse
 from input.forms import ProjectFactSheetForm
+import csv
 
 
 # Create your views here.
+def export_to_csv(request):
+    sheets = ProjectFactSheet.objects.all()
+    response = HttpResponse('text/csv')
+    response['Content-Disposition'] = 'attachment; filename=sheets_export.csv'
+    writer = csv.writer(response)
+    writer.writerow([
+        'project_ID',
+        'project_Phase',
+        'source_of_Funding',
+        'funding_Received',
+        'year_Funding_Received',
+        'project_Status_Update',
+        'links_Report_Studies',
+        'links_Images',
+        'input_POC_Last_Name',
+        'input_POC_First_Name',
+        'input_POC_Email',
+        'input_Date',
+        'approved_by_staff',
+    ])
+    sheet_fields = sheets.values_list(
+        'project_ID',
+        'project_Phase',
+        'source_of_Funding',
+        'funding_Received',
+        'year_Funding_Received',
+        'project_Status_Update',
+        'links_Report_Studies',
+        'links_Images',
+        'input_POC_Last_Name',
+        'input_POC_First_Name',
+        'input_POC_Email',
+        'input_Date',
+        'approved_by_staff',
+    )
+    for sheet in sheet_fields:
+        writer.writerow(sheet)
+    return response
+
+
 @login_required
 def project_fact_sheet_view(request):
     context = {}
