24e9bbee7af3b879310f5375728746753e8e42a4
Yamin Eigenheer <yeigenheer1@my.hpu.edu>
2022-11-15 14:23:39 -1000

    Add railway link to allowed host

--- dataonthewire/settings.py
+++ dataonthewire/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dataonthewire-production.up.railway.app','127.0.0.1', 'localhost']
 
 
 # Application definition
